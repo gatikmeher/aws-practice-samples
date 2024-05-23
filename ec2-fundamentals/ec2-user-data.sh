80e18cd22923077e1b66ea00bc141aa9ceb83491
Gati Krishna Meher <gatimeher@gmail.com>
2024-05-23 21:37:44 +0530

    added data

---
+++ ec2-fundamentals/ec2-user-data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+
+#!/bin/bash
+yum update -y
+sudo yum install java-17-amazon-corretto-headless -y
+sudo yum install ruby -y
+sudo yum install wget -y
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo systemctl start codedeploy-agent
+sudo reboot
+
+- IAM Role needs to be attached
